eef0488d5e3673af0bf713b09d76965a3f9c04fd
PhilaniMant <mancotywap@gmail.com>
2024-11-28 15:03:16 +0200

    completed homepage

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar');
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
